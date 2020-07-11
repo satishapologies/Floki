05e80ab9e2841545a3a983629a3cca59ea058221
satishapologies <42509238+satishapologies@users.noreply.github.com>
2020-07-11 22:19:40 +1000

    Add files via upload
    Added two more scripts. GetInventory and Email

---
+++ Email.ps1
@@ -0,0 +1,13 @@
+﻿# Send email it as a HTML body.
+$smtpServer = "smtp.gmail.com"
+$MailFrom = "satishapologies@gmail.com"
+$mailto = "satish_apologies@gmail.com"
+$msg = new-object Net.Mail.MailMessage 
+$smtp = new-object Net.Mail.SmtpClient($smtpServer) 
+$msg.From = $MailFrom
+$msg.IsBodyHTML = $true
+$msg.To.Add($Mailto) 
+$msg.Subject = "Report-Test."
+$MailTextT =  Get-Content  -Path C:\temp\Reports\Report.html
+$msg.Body = $MailTextT
+$smtp.Send($msg) 
